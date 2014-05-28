3efda285977050e21d7e22413904baff44eb5355
VitordeAquino <vitordeaquino@gmail.com>
2014-05-28 09:45:41 -0300

    configurando site

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,7 +92,7 @@ BolaoOficial::Application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default :charset => "utf-8"
 
-  config.action_mailer.default_url_options = {:host => 'http://bolao2014.herokuapp.com/'}
+  config.action_mailer.default_url_options = {:host => 'http://bolao2014teste.herokuapp.com/'}
   config.action_mailer.delivery_method = :smtp
   
   config.action_mailer.smtp_settings = {
